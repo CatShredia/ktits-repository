98ba68c40feb11fee477c96b1f157636e1f1339a
CatShredia <sasha990g@gmail.com>
2024-12-01 16:26:02 +0300

    updateProfHtmlCssJs0.2

--- prof/Okoprom/script.js
+++ prof/Okoprom/script.js
@@ -1,17 +1,20 @@
 let num = 0;
-let menuActive = document.querySelector(".menu-active");
+let menuActive = document.querySelector(".menu-active0");
+let menuButton = document.querySelector(".menu-button");
 function menu() {
   if (num == 0) {
     console.log("Открытие меню");
 
     menuActive.style.display = "flex";
+    menuButton.src = "rec/img/Close.png";
 
     num++;
   } else {
     console.log("Закрытие меню");
 
     menuActive.style.display = "none";
-
+    menuButton.src = "rec/img/Menu.png";
+    
     num--;
   }
 }
