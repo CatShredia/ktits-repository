e81c986d155a89f41baa6881d750fa419b8ed4c9
CatShredia <sasha990g@gmail.com>
2024-12-11 11:24:16 +0300

    prof1.0

--- prof/Okoprom/slider/script.js
+++ prof/Okoprom/slider/script.js
@@ -1,38 +1,78 @@
-class SliderClass {
-  //конструктор слайдера
-  constructor(sliderObject, countSlid) {
-    console.log("Объект Слайдера");
-    this.sliderObject = sliderObject;
-    console.log(sliderObject);
-    console.log("Количество слайдов на странице");
-    this.countSlid = countSlid;
-    console.log(countSlid);
+class Slider {
+  constructor(countSliderOnPage) {
+    this.countSliderOnPage = countSliderOnPage; //count of slid on page
+    this.slider = document.querySelector(".slider"); //slider object
+    this.slids = document.querySelectorAll(".slid"); //collection of slides
+    this.countHtmlSlides = this.slids.length; //count slides in html document
 
-    this.displaySliders();
+    this.start = 0; //display start
+    this.stop = countSliderOnPage; //display stop
 
-    this.buttonLeft = sliderObject.children[0];
-    this.buttonRight = sliderObject.children[countSlid + 1];
+    this.displaySlids(this.start, this.stop); //first display slide
+    this.addEventButton(); //events
+
+    // TODO: for devops
+    this.devMode = false; //dev mode
   }
-  switchLeft() {
-    console.log("Левая");
-  }
-  switchRight() {
-    console.log("Правая");
-  }
-  //отображение слайдов
-  displaySliders() {
-    for (let i = 0; i <= this.countSlid; i++) {
-      console.log(this.sliderObject.children[i]);
-      this.sliderObject.children[i].style.display = "flex";
+
+  //TODO: to diplay slides
+  displaySlids(start, stop) {
+    if (this.devMode) {
+      console.log("start: " + start);
+      console.log("stop: " + stop);
+    }
+
+    for (let i = 0; i < this.countHtmlSlides; i++) {
+      this.slids[i].style.display = "none";
+    }
+    for (let i = start; i < stop; i++) {
+      this.slids[i].style.display = "grid";
     }
   }
+  addEventButton() {
+    const buttons = this.slider.querySelectorAll(".slider-button");
+
+    buttons[0].addEventListener("click", (ivent) => {
+      this.switchLeft();
+    });
+
+    buttons[1].addEventListener("click", (ivent) => {
+      this.switchRight();
+    });
+  }
+  switchLeft() {
+    // console.log("left");
+
+    this.start -= 1;
+    this.stop -= 1;
+
+    if (this.start < 0) {
+      this.start = this.countHtmlSlides - 3;
+      this.stop = this.countHtmlSlides;
+    }
+
+    this.displaySlids(this.start, this.stop);
+  }
+  switchRight() {
+    // console.log("right");
+
+    this.start += 1;
+    this.stop += 1;
+
+    if (this.stop > this.countHtmlSlides) {
+      this.stop = 3;
+      this.start = 0;
+    }
+
+    this.displaySlids(this.start, this.stop);
+  }
 }
 
-function domLoaded() {
-  console.log("DOM loaded");
+document.addEventListener("DOMContentLoaded", (elem) => {
+  let sliderObject = new Slider(3);
 
-  //sliderObject - объект слайдера
-  //countSlid - количество слайдов на странице
-  let slider = new SliderClass(document.querySelector(".slider"), 3);
-}
-addEventListener("DOMContentLoaded", domLoaded());
+  // timer;
+  setInterval(function timerSlider() {
+    sliderObject.switchRight();
+  }, 6000);
+});
