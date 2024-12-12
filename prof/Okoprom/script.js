5c453b674df517b8b8e867d7b97d8a3e075817e0
CatShredia <sasha990g@gmail.com>
2024-12-12 13:36:36 +0300

    prof1.5

--- prof/Okoprom/script.js
+++ prof/Okoprom/script.js
@@ -1,20 +1,41 @@
 let num = 0;
 let menuActive = document.querySelector(".menu-active0");
-let menuButton = document.querySelector(".menu-button");
+let menuButton = document.querySelectorAll(".menu-button");
 
 function menu() {
   if (num == 0) {
     console.log("Открытие меню");
 
     menuActive.style.display = "flex";
-    menuButton.src = "rec/img/Close.png";
+    menuButton[0].src = "rec/img/Close.png";
 
     num++;
   } else {
     console.log("Закрытие меню");
 
     menuActive.style.display = "none";
-    menuButton.src = "rec/img/Menu.png";
+    menuButton[0].src = "rec/img/Menu.png";
+
+    num--;
+  }
+}
+const menuFooter = document.querySelector(".menu-footer");
+
+console.log(menuFooter);
+
+function menu2() {
+  if (num == 0) {
+    console.log("Открытие меню");
+
+    menuFooter.style.display = "flex";
+    menuButton[1].src = "rec/img/Close.png";
+
+    num++;
+  } else {
+    console.log("Закрытие меню");
+
+    menuFooter.style.display = "none";
+    menuButton[1].src = "rec/img/Menu.png";
 
     num--;
   }
