4e5b10e111bb41f40189f7e2106b1602f7ef9613
CatShredia <sasha990g@gmail.com>
2025-01-14 16:47:16 +0300

    oaip 14.01 0.1

---
+++ OAIP/OAIP_14_01.cs
@@ -0,0 +1,79 @@
+using System.Runtime.CompilerServices;
+
+namespace OAIP
+{
+    class OAIP_14_01
+    {
+        private bool isDevoperEdition;
+
+        public OAIP_14_01(bool isDevoperEdition)
+        {
+            this.isDevoperEdition = isDevoperEdition;
+
+            Console.WriteLine("---Обработка исключений---");
+
+            menu();
+        }
+
+        public static void menu() {
+            Console.WriteLine("Введите номер исключения (для выхода - 0):");
+            Console.WriteLine("FormatException \nInvalidOperationException \nOverflowException");
+
+            int number = 0;
+
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+            } 
+            catch (FormatException)
+            {
+                Console.WriteLine("Вы ввели не верное выражение");
+
+                menu();
+            }
+
+            switch (number)
+            {
+                case 0:
+                    break;
+                case 1:
+                    task1();
+                    break;
+                case 3:
+                    task3();
+                    break;
+                default:
+                    Console.WriteLine("Такого исключения нет");
+                    menu();
+                    break;
+            }
+        }
+        public static void task1() {
+            try
+            {
+                Console.WriteLine("Попробуйте ввести строку, число");
+                int number = Convert.ToInt32(Console.ReadLine());
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine("Вы ввели не верное выражение" + " " + e.Message);
+
+                task1();
+            }
+
+            menu();
+        }
+        public static void task3() {
+            int number = 43642367436743674367;
+
+            // try {
+                number++;
+                Console.WriteLine(number);
+            // } catch {
+
+            // }
+
+            menu();
+        }
+    }
+}
