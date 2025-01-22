835e2928ad0c12e10092f3e98d739540164e5a8f
CatShredia <sasha990g@gmail.com>
2025-01-22 11:06:30 +0300

    oaip fileSystem practise fix 2

--- OAIP/files/FilesPractise.cs
+++ OAIP/files/FilesPractise.cs
@@ -1,15 +1,22 @@
 namespace OAIP
 {
+    using System.Text.RegularExpressions;
+
     class FilesPractise
     {
-        public static string fio = "Evimov Aleksandr";
+        public static string fio = "Evimov Aleksandr" + "\n";
         public static string filePath = "";
         public static string path = "";
+
+        public const string OLDWORD = "старое";
+        public const string NEWWORD = "новое";
 
         public FilesPractise(bool isDevoperEdition)
         {
             Console.WriteLine("---Практика с файлами---");
             Console.WriteLine("21.01.2025");
+
+            path = Directory.GetCurrentDirectory() + "\\files\\texts";
 
             Menu();
 
@@ -32,6 +39,9 @@ namespace OAIP
                 case 2:
                     Task2();
                     break;
+                case 3:
+                    Task3();
+                    break;
                 default:
                     Menu();
                     break;
@@ -41,16 +51,14 @@ namespace OAIP
         public static void Task1()
         {
             Console.WriteLine("-Задание 1-");
-            path = Directory.GetCurrentDirectory() + "\\files\\texts";
 
-
-            CreateFileAndDirectory();
+            CreateFileAndDirectory("text");
 
             WriteFileInformation();
 
             Menu();
         }
-        public static void CreateFileAndDirectory()
+        public static void CreateFileAndDirectory(string nameFile)
         {
             Console.WriteLine(path);
 
@@ -61,19 +69,24 @@ namespace OAIP
                 Console.WriteLine("Папка создана: " + path);
             }
 
-            filePath = path + "\\text.txt";
+            filePath = path + "\\" + nameFile + ".txt";
             if (!File.Exists(filePath))
             {
                 using (File.Create(filePath)) { }
                 Console.WriteLine("Файл создан: " + filePath);
-
             }
+
+            PrintInformationToFile();
+            PrintInformationToFile();
+            PrintInformationToFile();
+            PrintInformationToFile();
+        }
+        public static void PrintInformationToFile()
+        {
             if (File.Exists(filePath))
             {
                 File.AppendAllText(filePath, fio);
-                File.AppendAllText(filePath, "\n ");
-                File.AppendAllText(filePath, fio);
-                Console.WriteLine("Файл заполнен: " + filePath);
+                Console.WriteLine("Файл перезаписан: " + filePath);
             }
         }
         public static void WriteFileInformation()
@@ -85,6 +98,53 @@ namespace OAIP
         public static void Task2()
         {
             Console.WriteLine("-Задание 2-");
+
+            CreateFileAndDirectory("example");
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(OLDWORD);
+                for (int i = 1; i <= 10; i++)
+                {
+                    writer.WriteLine(i);
+                }
+                writer.WriteLine(OLDWORD);
+                writer.WriteLine(OLDWORD);
+                writer.WriteLine(OLDWORD);
+
+            }
+
+            Console.WriteLine($"Файл {filePath} успешно создан и заполнен!");
+
+            Menu();
+        }
+
+        public static void Task3()
+        {
+            filePath = path + "\\" + "example" + ".txt";
+
+            Console.WriteLine(filePath + " путь!");
+            try
+            {
+                string fileContent = File.ReadAllText(filePath);
+
+                string newContent = Regex.Replace(fileContent, @"\b" + OLDWORD + @"\b", NEWWORD);
+
+                // Записываем измененный текст обратно в файл (перезаписывая его)
+                File.WriteAllText(filePath, newContent);
+
+                Console.WriteLine("Замена слов выполнена успешно.");
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл не найден.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Ошибка при работе с файлом: " + ex.Message);
+            }
+
+            Menu();
         }
     }
 }
