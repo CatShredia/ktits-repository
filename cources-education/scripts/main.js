3e41c3a9445a5f27a8214a104fee55ef248b4a02
CatShredia <sasha990g@gmail.com>
2025-03-04 16:05:46 +0300

    cources-education v0.6.1 navmenu animation and feedback-animation

--- cources-education/scripts/main.js
+++ cources-education/scripts/main.js
@@ -1,15 +1,34 @@
-console.log("hi");
-
-let menu = document.querySelector(".navmenu");
-
-function menuOpen(elem) {
-  console.log(menu);
-
-  menu.style.display = "grid";
-}
-
-function menuClose(elem) {
-  console.log(menu);
-
-  menu.style.display = "none";
-}
+$(document).ready(function () {
+  let $menu = $(".navmenu");
+  function menuOpen() {
+    $menu.addClass("show");
+  }
+  function menuClose() {
+    $menu.removeClass("show");
+  }
+  $(".icon__link").on("click", function () {
+    menuOpen();
+  });
+  $(".navmenu__close").on("click", function () {
+    menuClose();
+  });
+});
+$(document).ready(function () {
+  let $menu = $(".navmenu");
+  function menuOpen() {
+    $menu.addClass("show");
+  }
+  function menuClose() {
+    $menu.removeClass("show");
+  }
+  $(".icon__link").on("click", function () {
+    menuOpen();
+  });
+  $(".navmenu__close").on("click", function () {
+    menuClose();
+  });
+  // Обработчик для кнопки отзыва
+  $(".feedback__button").on("click", function () {
+    $(this).closest(".feedback").find(".feedback-content").slideToggle(300); // Плавное открытие/закрытие контента отзыва
+  });
+});
