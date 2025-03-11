1d392f8f21fd0c7320c0cfec7edc1d5aefd1e6fb
CatShredia <sasha990g@gmail.com>
2025-03-11 09:44:57 +0300

    cources-education v1.0.2 login-form validation patch

--- cources-education/scripts/login.js
+++ cources-education/scripts/login.js
@@ -1,4 +1,4 @@
-// login menu
+// login menu open
 $(document).ready(function () {
   const $loginButton = $("#login-button");
   const $loginModal = $("#login-modal");
@@ -9,6 +9,8 @@ $(document).ready(function () {
   });
 
   $closeButton.on("click", function () {
+    $(".error-message").remove();
+    $(".success-message").remove();
     $loginModal.removeClass("show");
   });
 
@@ -18,3 +20,73 @@ $(document).ready(function () {
     }
   });
 });
+
+// form validation
+let form = $("form");
+
+form.on("submit", function (event) {
+  event.preventDefault();
+
+  // чистим все предыдущие сообщения
+  $(".form-group").removeClass("error");
+  $(".error-message").remove();
+  $(".success-message").remove();
+
+  // получаем значения из формы
+  let username = $("#username").val();
+  let password = $("#password").val();
+  let email = $("#email").val();
+
+  let isValid = true;
+
+  // Валидируем name
+  if (username.length < 8 || username.length > 20) {
+    showError(
+      $("#username").closest(".form-group"),
+      "Имя пользователя должно быть от 8 до 20 символов."
+    );
+    isValid = false;
+  }
+
+  // Валидируем пароль
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,20}$/;
+  if (!passwordRegex.test(password)) {
+    showError(
+      $("#password").closest(".form-group"),
+      "Пароль должен быть от 8 до 20 символов и содержать цифру и спец. символ."
+    );
+    isValid = false;
+  }
+
+  // Валидируем почту
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    showError(
+      $("#email").closest(".form-group"),
+      "Пожалуйста, введите корректный email."
+    );
+    isValid = false;
+  }
+
+  if (isValid) {
+    showSuccess();
+  } else {
+    console.log("Form is invalid. Please correct the errors.");
+  }
+});
+
+function showError(element, message) {
+  element.addClass("error");
+  const errorMessageElement = $(
+    "<div class='error-message'>" + message + "</div>"
+  );
+  element.append(errorMessageElement);
+}
+
+function showSuccess() {
+  const successMessageElement = $(
+    "<div class='success-message'>Форма успешно отправлена!</div>"
+  );
+  $(".form__button").after(successMessageElement);
+}
