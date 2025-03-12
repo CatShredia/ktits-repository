f550290b470a98162cc2f46baddf36a685514664
CatShredia <sasha990g@gmail.com>
2025-03-12 14:35:22 +0300

    oaip seafight 0.5.8 repait patch 1

--- OAIP/02.28.seaFight/SeaFight.cs
+++ OAIP/02.28.seaFight/SeaFight.cs
@@ -1,62 +1,60 @@
+// Данный класс SeaFight реализует игровую логику для морского боя между игроком и ботом. 
+// Он управляет началом игры, обработкой выстрелов игрока, проверкой победителей и отображением состояния игры. 
+// Игрок и бот имеют свои уровни, которые отображают состояние их кораблей, и игра продолжается до тех пор, пока один из игроков не победит или не будет ничья.
 namespace OAIP
 {
     using static System.Console;
-
     class SeaFight : Object
     {
+        // игрок 
         public User User;
+        // бот
         public Bot Bot;
-
+        // идет ли игра
         public bool isGame;
-
         public SeaFight()
         {
-            // строки столбцы
-
             // создаем игрока и бота, и левелы вместе с ними
             User = new User("игрок");
             Bot = new Bot("бот");
+            // создаем левел бота, который видет игрок
             Bot.LevelFog.Ships = Bot.Level.Ships;
-
-            // Clear();
-
-            // выводим левелы уже с туманом
-            User.Level.PrintContent();
-            Bot.LevelFog.PrintContent();
-
             // начинаем игру
             isGame = true;
             Game();
-
             ReadKey();
         }
-
+        // цикл игры
         public void Game()
         {
             while (isGame)
             {
                 // выводим левелы
-                // Clear();
+                Clear();
                 User.Level.PrintContent();
                 Bot.Level.PrintContent();
                 Bot.LevelFog.PrintContent();
-
-                // выбираем точку удара
-                int[] damagePoint = User.ChoisePointToDamage();
-                // наносим удар
-                if (Bot.Damage(damagePoint))
-                {
-                    PrintWithColor("\n\nВы попали!\n", ConsoleColor.Black, ConsoleColor.Red);
-                    // проверка на уничтоженные корабли
-                    Bot.LevelFog.CheckDestroyesShips(isGame);
-                    User.Level.CheckDestroyesShips(isGame);
-
-                    CheckWinner();
-                }
-                else
-                {
-                    PrintWithColor("\n\nЭхх, промах...\n", ConsoleColor.Black, ConsoleColor.Gray);
-                }
+                UserShoot();
+            }
+        }
+        // выстрел игрока
+        public void UserShoot()
+        {
+            // выбираем точку удара
+            int[] damagePoint = User.ChoisePointToDamage();
+            // наносим удар
+            if (Bot.Damage(damagePoint))
+            {
+                PrintWithColor("\n\nВы попали!\n", ConsoleColor.Black, ConsoleColor.Red);
+                // проверка на уничтоженные корабли
+                Bot.LevelFog.CheckDestroyesShips(isGame);
+                User.Level.CheckDestroyesShips(isGame);
+                // проверка, есть ли победитель
+                CheckWinner();
+            }
+            else
+            {
+                PrintWithColor("\n\nЭхх, промах...\n", ConsoleColor.Black, ConsoleColor.Gray);
             }
         }
         // проверка, есть ли победитель
@@ -81,14 +79,12 @@ namespace OAIP
                 }
             }
         }
-
         // победа
         public void Victory(Player winner)
         {
             isGame = false;
             PrintWithColor($"\n\nПобеда за {winner.Name}, у него осталось {winner.CountLiveShips}\n", ConsoleColor.Black, ConsoleColor.Yellow);
         }
-
         // ничья
         public void Draw(Player drawPlayer1, Player drawPlayer2)
         {
--- OAIP/02.28.seaFight/Ship.cs
+++ OAIP/02.28.seaFight/Ship.cs
@@ -1,13 +1,19 @@
+// Класс Ship представляет собой корабль с палубами. Он содержит словарь Decks, который хранит палубы корабля, 
+// и целочисленное поле LiveDeck, которое указывает количество неуничтоженных палуб. 
+// Конструктор инициализирует словарь Decks пустым массивом.
 namespace OAIP
 {
+    using static System.Console;
+
     class Ship
     {
+        // палубы корабля
         public Dictionary<int, int[]> Decks;
-
+        // Количество неуничтоженных палуб
         public int LiveDeck;
         public Ship()
         {
-            Decks = [];
+            Decks = new Dictionary<int, int[]>();
         }
     }
 }
