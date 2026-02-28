7e1869e305ac499441ef509c9ce790749fe6e139
CatShredia <catshredia.990@gmail.com>
2026-02-28 10:32:53 +0300

    flutter test_application patch 4 home patch 1 solomon widget

--- flutter_application_1/lib/home.dart
+++ flutter_application_1/lib/home.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    const Center(child: Text('Home', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Search', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Sell', style: TextStyle(fontSize: 24))),
+    const Center(child: Text('Profile', style: TextStyle(fontSize: 24))),
+  ];
 
   Future<void> _logout(BuildContext context) async {
     await Supabase.instance.client.auth.signOut();
@@ -24,11 +39,34 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'Добро пожаловать!',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: SalomonBottomBar(
+        selectedItemColor: Colors.amber,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        currentIndex: _currentIndex,
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text('Home'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.search),
+            title: const Text('Search'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.shopping_basket),
+            title: const Text('Sell'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.person),
+            title: const Text('Profile'),
+          ),
+        ],
       ),
     );
   }
