e3001471c8c61f8f7f823e0c029054a8dc870a1e
CatShredia <catshredia.990@gmail.com>
2026-03-03 15:59:35 +0300

    unity arkanoid patch 2 ball_moving

---
+++ UnityProjects/Arkanoid/Assets/Scripts/BallController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool isActiveBalls;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+
+    public GameObject playerObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        ballInitialForce = new Vector2(100.0f, 300.0f);
+        isActiveBalls = false;
+
+        ballPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // следование за платформой до запуска
+        if (!isActiveBalls && playerObject != null)
+        {
+            ballPosition.x = playerObject.transform.position.x;
+            transform.position = ballPosition;
+        }
+
+        // проверка нажатия на пробел
+        if (Input.GetButtonDown("Jump") && !isActiveBalls)
+        {
+            rb.AddForce(ballInitialForce);
+            isActiveBalls = !isActiveBalls;
+        }
+    }
+}
