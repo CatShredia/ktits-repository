513b94e7da7acdf14b09486a727116d9ffe90926
CatShredia <catshredia.990@gmail.com>
2026-03-12 10:18:34 +0300

    unity arkanoid patch 11 music_player fix1

---
+++ UnityProjects/Arkanoid/Assets/Scripts/MusicPlayerUIController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MusicPlayerUIController : MonoBehaviour
+{
+
+    [SerializeField] private GameObject musicPlayerPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
