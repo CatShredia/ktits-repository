7493c0fee5ad0f2fda4b4c756b94ebde00fe879d
CatShredia <catshredia.990@gmail.com>
2026-03-24 17:18:52 +0300

    15practise benchmark init from docx document

---
+++ MyBenchmarkDotNet/Program.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text;
+
+public class StringTest
+{
+    string[] numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten" };
+
+    [Benchmark]
+    public string WithStringBuilder()
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string s in numbers)
+        {
+            stringBuilder.Append(s);
+            stringBuilder.Append(".");
+        }
+        return stringBuilder.ToString();
+    }
+
+    [Benchmark]
+    public string WithConcatenation()
+    {
+        string result = "";
+        foreach (string s in numbers) result = result + s + ".";
+        return result;
+    }
+
+    [Benchmark]
+    public string WithInterpolation()
+    {
+        string result = "";
+        foreach (string s in numbers) result = $"{result}{s}.";
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<StringTest>();
+    }
+}
